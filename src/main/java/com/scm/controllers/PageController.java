e689b91236ab89c3936fc0e804c8de04a4e17bdb
basudev96 <basudevshrama604@gmail.com>
2025-01-13 17:26:37 +0530

    set validation in the signup

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,7 @@ package com.scm.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import com.scm.helper.MessageType;
 import com.scm.services.UserServices;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 
 
@@ -76,11 +78,16 @@ public class PageController {
     
     //Processing register
     @RequestMapping(value = "/do-register", method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session) {
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rBindingResult, HttpSession session) {
         System.out.println("Registering user...");
         //fetch data
         System.out.println(userForm);
         //validate data
+
+        if(rBindingResult.hasErrors()){
+            System.out.println("Error: "+rBindingResult.toString());
+            return "register";
+        }        
         //save data to database
         // User user = User.builder()
         // .name(userForm.getName())
--- src/main/java/com/scm/forms/UserForm.java
+++ src/main/java/com/scm/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.scm.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,10 +17,20 @@ import lombok.ToString;
 @Builder
 @ToString
 public class UserForm {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String name;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is not valid")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
     private String password;
+    @NotBlank(message = "Phone number is required")
+    @Size(min = 8, max = 15, message = "Invalid phone number")
     private String phoneNumber;
+    @NotBlank(message = "About is required")
     private String about;
 
 }
