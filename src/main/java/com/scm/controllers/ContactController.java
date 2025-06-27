d63c1236fba80ebbd57be7c037c87d0d7c771256
basudev96 <basudevshrama604@gmail.com>
2025-06-27 17:45:33 +0530

    add contact form creation

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -3,13 +3,20 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.scm.forms.ContactForm;
+
+import org.springframework.ui.Model;
+
 @Controller
 @RequestMapping("/user/contacts")
 public class ContactController {
 
     @RequestMapping("/add")
     //add contact page: handler
-    public String addContactView() {
+    public String addContactView(Model model) {
+        
+        ContactForm contactForm = new ContactForm();
+        model.addAttribute("contactForm", contactForm);
         return "user/add_contact";
     }
 
