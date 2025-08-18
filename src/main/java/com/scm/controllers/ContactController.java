9644e64757c796ce992a14c9391d2217406818d9
basudev96 <basudevshrama604@gmail.com>
2025-08-18 16:28:00 +0530

    add contact backend code contact info save db

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -1,15 +1,30 @@
 package com.scm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.scm.entities.Contact;
+import com.scm.entities.User;
 import com.scm.forms.ContactForm;
+import com.scm.helper.Helper;
+import com.scm.services.ContactService;
+import com.scm.services.UserServices;
 
 import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/user/contacts")
 public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+    @Autowired
+    private UserServices userServices;
 
     @RequestMapping("/add")
     //add contact page: handler
@@ -18,6 +33,35 @@ public class ContactController {
         ContactForm contactForm = new ContactForm();
         model.addAttribute("contactForm", contactForm);
         return "user/add_contact";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String saveContact(@ModelAttribute ContactForm contactForm,Authentication authentication) {
+
+        String username = Helper.getEmailOfLoggedInUser(authentication);
+
+        User user = userServices.getUserByEmail(username);
+
+        //form - convert to entity
+        Contact contact = new Contact();
+        contact.setName(contactForm.getName());
+        contact.setEmail(contactForm.getEmail());
+        contact.setPhoneNumber(contactForm.getPhoneNumber());
+        contact.setAddress(contactForm.getAddress());
+        contact.setDescription(contactForm.getDescription());
+        contact.setFavorite(contactForm.isFavorite());
+        contact.setWebsiteLink(contactForm.getWebsiteLink());
+        contact.setLinkedInLink(contactForm.getLinkedInLink());
+        contact.setUser(user);
+
+
+        contactService.save(contact);
+
+        //process the form data
+        System.out.println(contactForm);
+
+        return "redirect:/user/contacts/add";
+
     }
 
     //edit contact page: handler
